4ac430d04672f84104b069db4962626394a4f88b
adsalihac <adsalihac@gmail.com>
2023-09-02 12:38:27 +0530

    fix node issue

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -93,67 +93,67 @@ const config = {
   //   locales: ["en"],
   // },
 
-  plugins: [
-    [
-      "pwa",
-      {
-        debug: isDeployPreview,
-        offlineModeActivationStrategies: [
-          "appInstalled",
-          "standalone",
-          "queryString",
-        ],
-        pwaHead: [
-          {
-            tagName: "link",
-            rel: "icon",
-            href: "img/logo.png",
-          },
-          {
-            tagName: "link",
-            rel: "manifest",
-            href: "manifest.json",
-          },
-          {
-            tagName: "meta",
-            name: "theme-color",
-            content: "rgb(37, 194, 160)",
-          },
-          {
-            tagName: "meta",
-            name: "apple-mobile-web-app-capable",
-            content: "yes",
-          },
-          {
-            tagName: "meta",
-            name: "apple-mobile-web-app-status-bar-style",
-            content: "#000",
-          },
-          {
-            tagName: "link",
-            rel: "apple-touch-icon",
-            href: "img/logo.png",
-          },
-          {
-            tagName: "link",
-            rel: "mask-icon",
-            href: "img/logo.png",
-            color: "rgb(62, 204, 94)",
-          },
-          {
-            tagName: "meta",
-            name: "msapplication-TileImage",
-            content: "img/logo.png",
-          },
-          {
-            tagName: "meta",
-            name: "msapplication-TileColor",
-            content: "#000",
-          },
-        ],
-      },
-    ],
-  ],
+  // plugins: [
+  //   [
+  //     "pwa",
+  //     {
+  //       debug: isDeployPreview,
+  //       offlineModeActivationStrategies: [
+  //         "appInstalled",
+  //         "standalone",
+  //         "queryString",
+  //       ],
+  //       pwaHead: [
+  //         {
+  //           tagName: "link",
+  //           rel: "icon",
+  //           href: "img/logo.png",
+  //         },
+  //         {
+  //           tagName: "link",
+  //           rel: "manifest",
+  //           href: "manifest.json",
+  //         },
+  //         {
+  //           tagName: "meta",
+  //           name: "theme-color",
+  //           content: "rgb(37, 194, 160)",
+  //         },
+  //         {
+  //           tagName: "meta",
+  //           name: "apple-mobile-web-app-capable",
+  //           content: "yes",
+  //         },
+  //         {
+  //           tagName: "meta",
+  //           name: "apple-mobile-web-app-status-bar-style",
+  //           content: "#000",
+  //         },
+  //         {
+  //           tagName: "link",
+  //           rel: "apple-touch-icon",
+  //           href: "img/logo.png",
+  //         },
+  //         {
+  //           tagName: "link",
+  //           rel: "mask-icon",
+  //           href: "img/logo.png",
+  //           color: "rgb(62, 204, 94)",
+  //         },
+  //         {
+  //           tagName: "meta",
+  //           name: "msapplication-TileImage",
+  //           content: "img/logo.png",
+  //         },
+  //         {
+  //           tagName: "meta",
+  //           name: "msapplication-TileColor",
+  //           content: "#000",
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // ],
 
   presets: [
     [
