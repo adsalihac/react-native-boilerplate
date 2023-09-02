08d4394a3c342e707cbfcae29eb48a8b2ee5ee13
adsalihac <adsalihac@gmail.com>
2023-09-02 20:12:39 +0530

    baseUrl removed

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -54,7 +54,7 @@ const config = {
 
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/react-native-boilerplate/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
