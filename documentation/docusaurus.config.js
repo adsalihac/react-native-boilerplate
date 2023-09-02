7effbc5a09ca9ebed2134ddff5899edf7ff1dd63
adsalihac <adsalihac@gmail.com>
2023-09-02 18:56:32 +0530

    code clean

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -82,8 +82,8 @@ const config = {
   markdown: {
     mermaid: true,
   },
-  onBrokenLinks: "warn",
-  onBrokenMarkdownLinks: "warn",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "throw",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
